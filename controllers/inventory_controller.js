499c08046ee1cc2c232defdbcec7d3830e9aa6d2
Ellis Delgado <ellisdelgado13@gmail.com>
2021-02-04 19:07:46 -0800

    Changed db, added code to user.js, messing around with controllers

---
+++ controllers/inventory_controller.js
@@ -0,0 +1,9 @@
+exports.index = (req, res) => res.render("inventory");
+//Calling invetory and findAll()
+
+const db = require("../models");
+
+exports.index = (req, res) => {
+    const query = {},
+
+}
---
+++ models/user.js
@@ -0,0 +1,24 @@
+const sequelize = require("sequelize");
+
+module.exports = (sequelize, DataTypes) => {
+    const User = sequelize.define("User", {
+        name: {
+            type: DataTypes.STRING,
+            allowNull: false,
+            validate: {
+                len: [1]
+            }
+        },
+        email: {
+            type: DataTypes.STRING,
+            allowNull: false,
+            validate: {
+                isEmail: true
+            }
+        },
+        password: {
+            type: DataTypes.STRING,
+            allowNull: false
+        },
+    })
+}
