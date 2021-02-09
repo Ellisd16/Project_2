79730486dce5c5cc29bc23e9398b709d104c037c
Ethan Newell <ethannewell@me.com>
2021-02-08 22:31:29 -0800

    I finally figured out what was going wrong with the sequelize and the handlebars, I also fixed some css links to some images hosted outside of the program

--- controllers/inventory_controller.js
+++ controllers/inventory_controller.js
@@ -7,18 +7,18 @@ const db = require("../models");
 
 exports.index = (req, res) => {
     console.log("we got to inventory_controller.index")
-    res.render('inventory/inventory', {
-        layout: 'main',
-    })
+    // res.render('inventory/inventory', {
+    //     layout: 'main',
+    // })
     console.log("it isnt the res function")
 
-    // db.stock.findAll().then(function (dbProduct) {
-    //     console.log("we got to the render function.")
-    //     res.render('inventory/inventory', {
-    //         layout: 'main',
-    //         product: dbProduct
-    //     });
-    //     console.log(dbProduct)
-    // });
+    db.Product.findAll().then(function (dbProduct) {
+        console.log("we got to the render function.")
+        res.render('inventory/inventory', {
+            layout: 'main',
+            product: dbProduct
+        });
+        console.log(dbProduct)
+    });
 
 };
