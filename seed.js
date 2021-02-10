dce1c5e86a890c6dea1dfc0c80650cf902c5bd1a
Ethan Newell <ethannewell@me.com>
2021-02-10 13:13:09 -0800

    seed

--- seed.js
+++ seed.js
@@ -4,11 +4,18 @@ const Sequelize = require("sequelize")
 
 const models = require('./models')
 
-const user = models.Admin.create({
+const user = models.Admin.destory({
+  where: {
     username: 'ethan123',
     email: 'this@gmail.com',
     password: 'th1sPassw0rd',
-});
+}});
+const user2 = models.Admin.destory({
+  where: {
+    username: 'Adm1nJJ35768912',
+    email: 'theonlyethannewell@gmail.com',
+    password: '142Ttjyy89465FHER',
+}});
   // let's assume the default of isAdmin is false
   console.log(user.username); // 'alice123'
   console.log(user.isAdmin); // false
