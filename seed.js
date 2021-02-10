f2173b859e2751fe8f172a4a255754636a04a699
Ethan Newell <ethannewell@me.com>
2021-02-10 13:20:49 -0800

    seed

--- seed.js
+++ seed.js
@@ -10,12 +10,13 @@ const user = models.Admin.destroy({
     email: 'this@gmail.com',
     password: 'th1sPassw0rd',
 }});
-const user2 = models.Admin.destory({
+const user2 = models.Admin.create({
   where: {
     username: 'Adm1nJJ35768912',
     email: 'theonlyethannewell@gmail.com',
     password: '142Ttjyy89465FHER',
 }});
+user2();
   // let's assume the default of isAdmin is false
   console.log(user.username); // 'alice123'
   console.log(user.isAdmin); // false
