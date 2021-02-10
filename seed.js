f319ad1f1cffdb89716c9cf40b099b7c738a9ce7
Ethan Newell <ethannewell@me.com>
2021-02-10 13:06:22 -0800

    seed

---
+++ seed.js
@@ -0,0 +1,14 @@
+const fs = require("fs");
+const path = require("path");
+const Sequelize = require("sequelize")
+
+const models = require('./models')
+
+const user = models.Admin.create({
+    username: 'ethan123',
+    email: 'this@gmail.com',
+    password: 'th1sPassw0rd',
+});
+  // let's assume the default of isAdmin is false
+  console.log(user.username); // 'alice123'
+  console.log(user.isAdmin); // false
